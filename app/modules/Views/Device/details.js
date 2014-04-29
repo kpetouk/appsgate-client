0a72b6c596d4116b80045909278d3037170eada8
Thibad Flury <thibaud.flury@imag.fr>
2014-04-29 15:26:06 +0200

    bugfix, blink function is a remote Control, not a remoteCall

--- app/modules/Views/Device/details.js
+++ app/modules/Views/Device/details.js
@@ -102,7 +102,7 @@ define([
             e.preventDefault();
             var lamp = devices.get($(e.currentTarget).attr("id"));
             // send the message to the backend
-            lamp.remoteCall("blink", []);
+            lamp.remoteControl("blink", []);
 
             return false;
         },
